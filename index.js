69a4871ba87701e2f8c474b637f37fa49f8cf6aa
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-22 20:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ var uppercase = string.toUpperCase()
 
 if (string === lowercase) {
   return "I can't hear you!"
-  
+}  
 else if (string === uppercase) {
   return "YES INDEED!"
-
+}
+else if (string === "I love you, Grandma.") {
+  return "I love you, too."
+}
+}
